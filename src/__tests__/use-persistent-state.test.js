530e5a74010ccb9d1782d0e7c9a47381607b91e8
Jonas Gierer <jonas@gierer.xyz>
2019-05-05 16:53:48 +0200

    Add usePersistentState hook

---
+++ src/__tests__/use-persistent-state.test.js
@@ -0,0 +1,47 @@
+import { renderHook } from "./test-utils";
+import { act } from "react-hooks-testing-library";
+
+import { usePersistentState } from "../use-persistent-state";
+
+const convertState = ([value, setValue]) => ({ value, setValue });
+
+beforeEach(() => {
+  console.error = jest.fn();
+});
+
+afterEach(() => {
+  window.localStorage.clear();
+});
+
+test(`Acts like useState`, () => {
+  const key = `key`;
+  const value1 = `hello`;
+  const value2 = `world`;
+
+  const { result } = renderHook(() => usePersistentState(key, value1));
+  expect(convertState(result.current).value).toBe(value1);
+  act(() => convertState(result.current).setValue(value2));
+  expect(convertState(result.current).value).toBe(value2);
+});
+
+test(`Gets initial value from localStorage`, () => {
+  const key = `key`;
+  const value1 = `hello`;
+  const value2 = `world`;
+
+  window.localStorage.setItem(key, JSON.stringify(value2));
+  const { result } = renderHook(() => usePersistentState(key, value1));
+
+  expect(convertState(result.current).value).toBe(value2);
+});
+
+test(`Saves new values to localStorage`, () => {
+  const key = `key`;
+  const value1 = `hello`;
+  const value2 = `world`;
+
+  const { result } = renderHook(() => usePersistentState(key, value1));
+  expect(JSON.parse(window.localStorage.getItem(key))).toBe(value1);
+  act(() => convertState(result.current).setValue(value2));
+  expect(JSON.parse(window.localStorage.getItem(key))).toBe(value2);
+});
---
+++ src/use-persistent-state.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+
+const safeStorage = (action, ...args) => {
+  try {
+    return window.localStorage[action](...args);
+  } catch (error) {
+    console.error(`Error while trying to access localStorage: ${error}`);
+  }
+};
+
+const safeJSON = (action, ...args) => {
+  try {
+    return JSON[action](...args);
+  } catch (error) {
+    console.error(`Error while trying to ${action} JSON: ${error}`);
+  }
+};
+
+/**
+ * `useStorage` while persisting the state to `localStorage` across sessions
+ * @param {String} key The key under which the value should be stored
+ * @param {*} initialValue Fallback initial value. Will be overwritten by value from `localStorage` if available
+ *
+ * @example
+ * const [name, setName] = usePersistentState(`name`, `Mike`);
+ * setName(`Jonas`); // `name` will be initialized to `Jonas` instead of `Mike` in all future sessions
+ */
+export const usePersistentState = (key, initialValue) => {
+  const [value, setValue] = React.useState(() => {
+    const json = safeStorage(`getItem`, key);
+    return safeJSON(`parse`, json) || initialValue;
+  });
+
+  React.useEffect(() => {
+    safeStorage(`setItem`, key, safeJSON(`stringify`, value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
