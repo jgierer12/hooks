fa3384d293e2ec63c4c4ec8d5f043a77c17a97df
Jonas Gierer <jonas@gierer.xyz>
2019-05-05 16:55:12 +0200

    Export usePersistentState

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { useEventListener } from "./use-event-listener";
 export { useMount } from "./use-mount";
+export { usePersistentState } from "./use-persistent-state";
 export { useRerender } from "./use-rerender";
 export { useTimeout } from "./use-timeout";
